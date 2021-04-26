cca65a1a456a372951a65d4ee6dc1f282e88ec7c
mathesbh <mathesbh@gmail.com>
2021-04-26 16:07:28 -0300

    Adiciona componente pesquisar

---
+++ src/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input type="text" placeholder="Buscar">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  input{
+    padding: 10px;
+    margin-bottom: 2%;
+  }
+</style>
