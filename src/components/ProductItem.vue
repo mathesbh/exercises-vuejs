87cf437907a26c1f9ed97584c7bcf11a28ca37b9
mathesbh <mathesbh@gmail.com>
2021-04-26 16:06:44 -0300

    Adiciona componente productItem

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <h4>{{ product.name }}</h4>
+      <p>R${{ product.price }}</p>
+      <p>{{ product.category }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+  props: {
+    product: Object,
+  },
+
+};
+</script>
+
+<style scoped>
+
+.box{
+  border: 3px solid white;
+  text-align: center;
+  margin: 5px 0;
+  padding: 10px;
+}
+</style>
