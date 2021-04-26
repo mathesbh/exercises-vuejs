740588112053545ab03104d3b106e09e95d0a76c
mathesbh <mathesbh@gmail.com>
2021-04-26 16:07:57 -0300

    Inclui componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <ProductForm :onAddProduct="handleOnAddProduct"/>
+    <div class="box">
+      <Search />    
+      <ProductList :products="products"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductForm from './components/ProductForm';
+import ProductList from './components/ProductList';
+import Search from './components/Search';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductForm,
+    ProductList,
+    Search,
+  },
+  data(){
+    return{
+      products: [
+        {
+          name: 'Iphone 12',
+          price: '10000.00',
+          category: 'Smartphone',
+        },
+        {
+          name: 'Samsung Galaxy',
+          price: '5000.00',
+          category: 'Smartphone',
+        },
+      ]
+    }
+  },
+  methods: {
+    handleOnAddProduct(product){
+      this.products = this.products.concat(product);
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .container{
+    display: flex;
+    justify-content: space-evenly;
+  }
 </style>
