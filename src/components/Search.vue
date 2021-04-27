9e91e42597b96a4258dfa8d95e290e69177bbe53
mathesbh <mathesbh@gmail.com>
2021-04-26 21:14:46 -0300

    Adiciona filtro de produtos

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <input type="text" placeholder="Buscar">
+    <input type="text" placeholder="Buscar" v-model="search" v-on:keyup="handleSearch">
   </div>
 </template>
 
 <script>
 export default {
+  name: "Search",
+  components: {
 
+  },
+  props: {
+    onSearch: Function,
+  },
+  data(){
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    handleSearch(){
+      this.onSearch(this.search);
+    }
+  }
 }
 </script>
 
