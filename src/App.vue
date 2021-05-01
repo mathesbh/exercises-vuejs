5244b3dd24caab51c005deab2b1b73fa0e7e482e
mathesbh <mathesbh@gmail.com>
2021-04-30 22:45:22 -0300

    Modifica app para utilizar store e router-view

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,19 @@
 <template>
   <div class="container">
-    <ProductForm :onAddProduct="handleOnAddProduct"/>
-    <div class="box">
-      <Search :onSearch="handleSearchProducts"/>    
-      <ProductList :products="listedProducts.length > 0 ? listedProducts : products"/>
-    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import ProductForm from './components/ProductForm';
-import ProductList from './components/ProductList';
-import Search from './components/Search';
-
 export default {
   name: 'App',
   components: {
-    ProductForm,
-    ProductList,
-    Search,
   },
   data(){
     return{
-      products: [],
-      listedProducts: [],
     }
   },
   methods: {
-    handleOnAddProduct(product){
-      this.products = this.products.concat(product);
-    },
-    handleSearchProducts(text){
-      const results = this.products.filter((e) => e.name.startsWith(text));
-      if(results.length > 0){
-        this.listedProducts = results;
-      } else{
-        this.listedProducts = this.products;
-      }
-    }
   }
 }
 </script>
